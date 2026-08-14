4a7428006408de407d19ea07068e276092ac65fa
Xing Yu Yi <yixingyu05@gmail.com>
2026-08-14 14:35:26 +0100

    Updated from main

--- Ekman/3D Simulation/Overnight_TKE.jl
+++ Ekman/3D Simulation/Overnight_TKE.jl
@@ -3,11 +3,11 @@
 r = nothing
 profile = nothing
 
-file = "Ekman 3D.jl"
+file = "TKE.jl"
 
-for p in [4]
+for p in [0,4]
 global profile = p      # set bᵢ (0=linear, 1=nonlinear, 2=exponential, 3=linear+exp decay, 4=softplus)
-    for ratio in [1,2,5,10]
+    for ratio in [0,0.5]
         global r = ratio
 
         if profile == 0         # linear
@@ -31,7 +31,7 @@ global profile = p      # set bᵢ (0=linear, 1=nonlinear, 2=exponential, 3=line
             end
 
         elseif profile == 4     # softplus
-            for value in [40, 50]
+            for value in [5,10,15,20,30,40,50]
                 global T = value
                 include(file)
             end
