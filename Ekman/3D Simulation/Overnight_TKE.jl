304bd734233d122cfb0e87a197116981177b07ac
Xing Yu Yi <yixingyu05@gmail.com>
2026-08-18 14:25:51 +0100

    Plots

--- Ekman/3D Simulation/Overnight_TKE.jl
+++ Ekman/3D Simulation/Overnight_TKE.jl
@@ -19,7 +19,7 @@ global profile = p      # set bᵢ (0=linear, 1=nonlinear, 2=exponential, 3=line
             end
 
         elseif profile == 2     # exponential with fixed buoyancy difference of N
-            for value in [10, 20, 40, 60]
+            for value in [5, 10, 20, 30, 40, 60]
                 global Lᴰ = value
                 include(file)
             end
