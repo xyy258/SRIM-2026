4ed1618191bb573ad133c2d731510db79ea52611
Xing Yu Yi <yixingyu05@gmail.com>
2026-08-20 12:03:28 +0100

    Linear profile update

--- Ekman/3D Simulation/Overnight.jl
+++ Ekman/3D Simulation/Overnight.jl
@@ -5,9 +5,9 @@ profile = nothing
 
 file = "Ekman 3D.jl"
 
-for p in [4]
+for p in [0]
 global profile = p      # set bᵢ (0=linear, 1=nonlinear, 2=exponential, 3=linear+exp decay, 4=softplus)
-    for ratio in [0.5,1,2,10]
+    for ratio in [0,0.5,1,2,5,10]
         global r = ratio
 
         if profile == 0         # linear
